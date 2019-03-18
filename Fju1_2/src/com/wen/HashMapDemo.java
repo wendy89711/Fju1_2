d6387b1b78f10e2289478b31f0624c4c2b94d61c
Wendy Wu <wendy89711@gmail.com>
2019-03-19 00:54:38 +0800

    temperature

---
+++ Fju1_2/src/com/wen/HashMapDemo.java
@@ -0,0 +1,35 @@
+package com.wen;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		Map<String, String> hmap = new HashMap<>();
+		hmap.put("張三谷", "牧師");
+		Map<String, String> post = Map.of("何九山","教師","李四裴","警察","周六圖","教師");
+		hmap.putAll(post);
+		System.out.println("集合內容:" + hmap);
+		System.out.println("hmap集合的元素個數:" + hmap.size());
+		
+		System.out.println("\n加入\"何九山\"...");
+		if(hmap.containsKey("何九山")) {
+			System.out.println("\"何九山\"已存在，重複加入時職業欄資料會被覆蓋。");
+		}
+		hmap.put("何九山", "軍人");
+		System.out.println("\"何九山\"的職業更改為:"+ hmap.get("何九山"));
+		System.out.println("加入\"曹五操\"...");
+		String name = "曹五操";
+		if(hmap.containsKey(name)) {
+			System.out.println("\"+name+\"+已存在，不接受更改。");
+		}else {
+			hmap.put(name, "律師");
+		}System.out.println("\n集合內容:");
+		for(Map.Entry e : hmap.entrySet()) {
+			System.out.println(e.getKey()+"="+e.getValue());
+		}
+		
+	}
+
+}
---
+++ Fju1_2/src/com/wen/Tem.java
@@ -0,0 +1,20 @@
+package com.wen;
+
+import java.util.HashMap;
+
+public class Tem {
+
+	public static void main(String[] args) {
+		HashMap<Integer, Integer> m = new HashMap<>();
+		for(int i = 0; i<24; i++) {
+			int tem = (int)(Math.random()*7);
+			tem += 28;
+			Integer freq = m.get(tem);
+			m.put(tem, freq == null ? 1: freq + 1);
+			//null是則1不是則freq+1
+			System.out.println(i + "點溫度" + tem + "度C");
+		}
+
+	}
+
+}
