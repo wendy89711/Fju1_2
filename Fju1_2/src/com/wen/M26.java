4f546e4c479bfd17881a7c7b9a110cb5f9277b5b
Wendy Wu <wendy89711@gmail.com>
2019-04-15 00:29:30 +0800

    4/14

--- Fju1_2/src/com/wen/M26.java
+++ Fju1_2/src/com/wen/M26.java
@@ -1,9 +1,29 @@
 package com.wen;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class M26 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Map<String, String> buy =  new HashMap<>();
+		Scanner sca = new Scanner(System.in);
+		String thing;
+		String amount;
+		int i = 1;
+		while (i>=1) {
+			System.out.println("新增代購物：");
+			thing = sca.nextLine();
+			if(!"q".equals(thing)) {
+				System.out.println("新增代購物數量：");
+				amount = sca.nextLine();
+				buy.put(thing, amount);
+				System.out.println(buy);
+			}else {
+				break;
+			}
+		}
 
 	}
 
