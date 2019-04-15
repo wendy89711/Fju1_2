437ca6638536bb8a8cfbc39ff60a00b9e308fbd5
Wendy Wu <wendy89711@gmail.com>
2019-04-15 20:44:03 +0800

    new package

---
+++ Fju1_2/src/com/classes/Item.java
@@ -0,0 +1,10 @@
+package com.classes;
+
+public class Item {
+	String name;
+	int qty;
+	public Item(String name, int qty) {
+		this.name = name;
+		this.qty = qty;
+	}
+}
---
+++ Fju1_2/src/com/classes/M26T.java
@@ -0,0 +1,31 @@
+package com.classes;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class M26T {
+
+	public static void main(String[] args) {
+		List<Item> items = new ArrayList<>();
+		Scanner sca = new Scanner(System.in);
+		String name = "";
+		int i = 1;
+		while (i >= 1) {
+			System.out.println("Please enter item name:");
+			name = sca.nextLine();
+			if (!"q".equals(name)) {
+				System.out.println("Please enter quantity:");
+				int qty = Integer.parseInt(sca.nextLine());
+				items.add(new Item(name, qty));
+				for (Item item : items) {
+					System.out.println(item.name + "\t" + item.qty);
+				}
+			} else {
+				break;
+			}
+		}
+
+	}
+
+}
